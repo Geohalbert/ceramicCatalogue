282c7ec8a645a219b29a950f701fe47880982035
Hal Peters <geohalbert@gmail.com>
2025-11-05 17:42:51 -0600

    added png to home screen and styled screen

--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -1,15 +1,33 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 import PotteryTile from "../components/PotteryTile"; 
 import AddItemButton from "../components/AddItemButton"; 
+import { StyleSheet } from "react-native";
 
 
 export default function Home() {
   return (
-    <View>
-        <Text>Home</Text>
-        <Text>Welcome to the Ceramic Catalogue</Text>
+    <View style={styles.container}>
+        <Text style={styles.title}>Welcome to the Ceramic Catalogue</Text>
+        <Image source={require('../../assets/home_screen_vase_cropped_300w.png')} style={styles.image} />
         <AddItemButton />
         <PotteryTile />
     </View>
   );    
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: 200,
+    height: 261,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  }
+});
