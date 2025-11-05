d20dc89548748ce36186a932bf5b512c1cd5433f
Hal Peters <geohalbert@gmail.com>
2025-11-05 14:52:46 -0600

    add AddItem screen and button

---
+++ components/AddItemButton.tsx
@@ -0,0 +1,11 @@
+import { Button, View } from "react-native";
+
+export default function AddItemButton() {
+
+  return (
+    <View>
+        {/* TODO: Add functionality to navigate to the AddItem screen */}
+        <Button title="Add Item" onPress={() => null} />   
+    </View>
+  );
+}
