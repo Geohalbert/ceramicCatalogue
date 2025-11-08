1fc419d947d7cda632a6e0d9a0baf6e2941e490a
Hal Peters <geohalbert@gmail.com>
2025-11-07 18:07:08 -0600

    removed header from the home screen

--- app/navigation/StackNavigator.tsx
+++ app/navigation/StackNavigator.tsx
@@ -9,7 +9,11 @@ const Stack = createNativeStackNavigator();
 export default function StackNavigator() {
     return (
         <Stack.Navigator>
-            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen 
+                name="Home" 
+                component={Home} 
+                options={{ headerShown: false }}
+            />
             <Stack.Screen name="AddItem" component={AddItem} />
             <Stack.Screen name="Collection" component={Collection} />
         </Stack.Navigator>
